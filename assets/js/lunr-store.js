1137e831738681921159121a3b7243710f0e2602
emanderson811 <emanders@iastate.edu>
2026-06-23 16:09:37 -0500

    add thumbs and highlight to lunr search results page

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -11,6 +11,9 @@ var store = [
 {%- for item in items -%} 
 {  
 {% for f in fields %}{% if item[f.field] %}{{ f.field | jsonify }}: {{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }},{% endif %}{% endfor %} 
+"objectid": {{ item.objectid | jsonify }},
+{% if item.image_thumb %}"image_thumb": {{ item.image_thumb | jsonify }},{% endif %}
+"image_alt_text": {{ item.image_alt_text | default: item.description | default: item.title | normalize_whitespace | replace: '""','"' | jsonify }},
 "id": {% if item.parentid %}{{ item.parentid | append: '.html#' | append: item.objectid | jsonify }}{% else %}{{item.objectid | append: '.html' | jsonify }}{% endif %}
 
 }{%- unless forloop.last -%},{%- endunless -%}
