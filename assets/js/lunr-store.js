f112854731a5ca62b38b91635527e904dd019ec0
emanderson811 <emanders@iastate.edu>
2026-09-03 12:17:09 -0500

    fix search results thumbnail URLs

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -12,7 +12,7 @@ var store = [
 {  
 {% for f in fields %}{% if item[f.field] %}{{ f.field | jsonify }}: {{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }},{% endif %}{% endfor %} 
 "objectid": {{ item.objectid | jsonify }},
-{% if item.image_thumb %}"image_thumb": {{ item.image_thumb | jsonify }},{% endif %}
+{% if item.image_thumb %}"image_thumb": {{ item.image_thumb | relative_url | jsonify }},{% endif %}
 "image_alt_text": {{ item.image_alt_text | default: item.description | default: item.title | normalize_whitespace | replace: '""','"' | jsonify }},
 "id": {% if item.parentid %}{{ item.parentid | append: '.html#' | append: item.objectid | jsonify }}{% else %}{{item.objectid | append: '.html' | jsonify }}{% endif %}
 
